8dfb45daa309ab965ce993a91a8f300a570a36f1
Meeseeks <41309640+GiselleMudge@users.noreply.github.com>
2018-12-03 13:35:55 -0500

    trying again

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000 || process.env.PORT, process.env.IP, function(){
-   console.log("Our Server is alive!");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Our Server is alive!");
 });
