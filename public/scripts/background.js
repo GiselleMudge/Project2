fc6830f42c690dc6f50e522f3092016659fcd5f3
Meeseeks <41309640+GiselleMudge@users.noreply.github.com>
2018-12-02 19:38:10 -0500

    Initial commit

---
+++ public/scripts/background.js
@@ -0,0 +1,22 @@
+var counter = 0;
+function changeBG(){
+    var imgs = [
+        "url(https://images.unsplash.com/photo-1505852679233-d9fd70aff56d?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1456791563204-0db6ac638604?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1539966903171-89770f33f468?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1468818438311-4bab781ab9b8?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1541282704585-448bb156dbfa?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1516466723877-e4ec1d736c8a?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1495458200315-8a1dce4bbe77?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)"
+      ]
+    
+    if(counter === imgs.length) counter = 0;
+    $("body").css("background-image", imgs[counter]);
+
+    counter++;
+}
+  
+  setInterval(changeBG, 2000);
+
+
